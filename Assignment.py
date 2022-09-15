6560c66cb8e0c3edeeb3958a05bb7a9f4859ca0a
ArchanaBiju <112687253+ArchanaBiju@users.noreply.github.com>
2022-09-15 20:20:03 +0530

    Delete Assignment.py

--- Assignment.py
+++
@@ -1,9 +0,0 @@
-for num in range(1,100):
-    count=0
-    for i in range (2, (num//2+1)):
-       if(num%i==0):
-          count = count+1
-          break
-
-    if(count == 0 and num !=1 ):
-        print("%d" %num)
